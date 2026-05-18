0bd7651705ada8529fb933d1a0b5252469867b86
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-18 14:09:07 -0700

    lvk | Enabled screenshot tests on Linux CI
    Reviewed By: fschliem
    
    Differential Revision: D105415448
    
    fbshipit-source-id: 0f9180ebe6bf125feb4da58ea654b498400fce0b

--- samples/Bistro.h
+++ samples/Bistro.h
@@ -153,20 +153,21 @@ bool loadAndCache(const std::string& folderContentRoot, const char* cacheFileNam
     // 1. Generate an index buffer
     const size_t indexCount = vertexData_.size();
     std::vector<uint32_t> remap(indexCount);
-    const size_t vertexCount =
+    const size_t remappedVertexCount =
         meshopt_generateVertexRemap(remap.data(), nullptr, indexCount, vertexData_.data(), indexCount, sizeof(VertexData));
     // 2. Remap vertices
     std::vector<VertexData> remappedVertices;
     indexData_.resize(indexCount);
-    remappedVertices.resize(vertexCount);
+    remappedVertices.resize(remappedVertexCount);
     meshopt_remapIndexBuffer(indexData_.data(), nullptr, indexCount, &remap[0]);
     meshopt_remapVertexBuffer(remappedVertices.data(), vertexData_.data(), indexCount, sizeof(VertexData), remap.data());
     vertexData_ = remappedVertices;
     // 3. Optimize for the GPU vertex cache reuse and overdraw
-    meshopt_optimizeVertexCache(indexData_.data(), indexData_.data(), indexCount, vertexCount);
+    meshopt_optimizeVertexCache(indexData_.data(), indexData_.data(), indexCount, remappedVertexCount);
     meshopt_optimizeOverdraw(
-        indexData_.data(), indexData_.data(), indexCount, &vertexData_[0].position.x, vertexCount, sizeof(VertexData), 1.05f);
-    meshopt_optimizeVertexFetch(vertexData_.data(), indexData_.data(), indexCount, vertexData_.data(), vertexCount, sizeof(VertexData));
+        indexData_.data(), indexData_.data(), indexCount, &vertexData_[0].position.x, remappedVertexCount, sizeof(VertexData), 1.05f);
+    meshopt_optimizeVertexFetch(
+        vertexData_.data(), indexData_.data(), indexCount, vertexData_.data(), remappedVertexCount, sizeof(VertexData));
   }
 
   // loop over materials
