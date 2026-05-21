796bb45eabd7750073f495a4b5908ee405fe0d5f
Sergey Kosarevsky <sergkosarevsky@meta.com>
2026-05-21 14:21:47 -0700

    lvk | Fix -Werror,-Wunused-variable in LVK.cpp
    Reviewed By: rudybear
    
    Differential Revision: D105994052
    
    fbshipit-source-id: 4cb5fc3017543dd56c39a4d802119d1b0ffd7de1

--- lvk/LVK.cpp
+++ lvk/LVK.cpp
@@ -172,9 +172,7 @@ uint32_t lvk::getTextureBytesPerLayer(uint32_t width, uint32_t height, lvk::Form
 }
 
 uint32_t lvk::getTextureBytesPerPlane(uint32_t width, uint32_t height, lvk::Format format, uint32_t plane) {
-  [[maybe_unused]] const TextureFormatProperties props = properties[format];
-
-  LVK_ASSERT(plane < props.numPlanes);
+  LVK_ASSERT(plane < properties[format].numPlanes);
 
   switch (format) {
   case Format_YUV_NV12:
