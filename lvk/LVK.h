a3aa2e8b27f752689b03d76e27166d191dd9babb
Sergey Kosarevsky <sk@linderdaum.com>
2026-05-31 04:59:01 -0700

    Use `Pool<>` from `ldrutils`

--- lvk/LVK.h
+++ lvk/LVK.h
@@ -16,6 +16,8 @@
 #include <memory>
 #include <utility>
 
+#include <ldrutils/lutils/Handle.h>
+
 // clang-format off
 #if defined(LVK_WITH_MINILOG)
   #include <minilog/minilog.h>
@@ -87,68 +89,16 @@ class IContext;
 
 bool Assert(bool cond, const char* file, int line, const char* format, ...);
 
-// Non-ref counted handles; based on:
-// https://enginearchitecture.realtimerendering.com/downloads/reac2023_modern_mobile_rendering_at_hypehype.pdf
-template<typename ObjectType>
-class Handle final {
- public:
-  Handle() = default;
-  explicit Handle(void* ptr)
-  : index_(reinterpret_cast<ptrdiff_t>(ptr) & 0xffffffff)
-  , gen_((reinterpret_cast<ptrdiff_t>(ptr) >> 32) & 0xffffffff) {}
-
-  bool empty() const {
-    return gen_ == 0;
-  }
-  bool valid() const {
-    return gen_ != 0;
-  }
-  uint32_t index() const {
-    return index_;
-  }
-  uint32_t gen() const {
-    return gen_;
-  }
-  void* indexAsVoid() const {
-    return reinterpret_cast<void*>(static_cast<ptrdiff_t>(index_));
-  }
-  void* handleAsVoid() const {
-    static_assert(sizeof(void*) >= sizeof(uint64_t));
-    return reinterpret_cast<void*>((static_cast<ptrdiff_t>(gen_) << 32) + static_cast<ptrdiff_t>(index_));
-  }
-  bool operator==(const Handle<ObjectType>& other) const {
-    return index_ == other.index_ && gen_ == other.gen_;
-  }
-  bool operator!=(const Handle<ObjectType>& other) const {
-    return index_ != other.index_ || gen_ != other.gen_;
-  }
-  // allow conditions 'if (handle)'
-  explicit operator bool() const {
-    return gen_ != 0;
-  }
-
- private:
-  Handle(uint32_t index, uint32_t gen) : index_(index), gen_(gen) {};
-
-  template<typename ObjectType_, typename ImplObjectType>
-  friend class Pool;
-
-  uint32_t index_ = 0;
-  uint32_t gen_ = 0;
-};
-
-static_assert(sizeof(Handle<class Foo>) == sizeof(uint64_t));
-
 // specialized with dummy structs for type safety
-using ComputePipelineHandle = lvk::Handle<struct ComputePipeline>;
-using RenderPipelineHandle = lvk::Handle<struct RenderPipeline>;
-using RayTracingPipelineHandle = lvk::Handle<struct RayTracingPipeline>;
-using ShaderModuleHandle = lvk::Handle<struct ShaderModule>;
-using SamplerHandle = lvk::Handle<struct Sampler>;
-using BufferHandle = lvk::Handle<struct Buffer>;
-using TextureHandle = lvk::Handle<struct Texture>;
-using QueryPoolHandle = lvk::Handle<struct QueryPool>;
-using AccelStructHandle = lvk::Handle<struct AccelerationStructure>;
+using ComputePipelineHandle = ldr::Handle<struct ComputePipeline>;
+using RenderPipelineHandle = ldr::Handle<struct RenderPipeline>;
+using RayTracingPipelineHandle = ldr::Handle<struct RayTracingPipeline>;
+using ShaderModuleHandle = ldr::Handle<struct ShaderModule>;
+using SamplerHandle = ldr::Handle<struct Sampler>;
+using BufferHandle = ldr::Handle<struct Buffer>;
+using TextureHandle = ldr::Handle<struct Texture>;
+using QueryPoolHandle = ldr::Handle<struct QueryPool>;
+using AccelStructHandle = ldr::Handle<struct AccelerationStructure>;
 
 // forward declarations to access incomplete type IContext
 void destroy(lvk::IContext* ctx, lvk::ComputePipelineHandle handle);
