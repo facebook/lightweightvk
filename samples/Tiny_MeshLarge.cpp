ac16eb5cd9d3fec0ba6e780e099e62b33e430ee4
generatedunixname910664361436750 <noreply+910664361436750@fb.com>
2026-06-22 13:20:15 -0700

    Fix CQS signal facebook-hte-UnqualifiedCall-abs in xplat/lightweightvk/public/samples
    Differential Revision: D109140518
    
    fbshipit-source-id: 4732cb750300988bb50e340cff763d94d629ac4f

--- samples/Tiny_MeshLarge.cpp
+++ samples/Tiny_MeshLarge.cpp
@@ -529,8 +529,8 @@ uint16_t packSnorm2x8(vec2 v) {
 
 // https://www.shadertoy.com/view/llfcRl
 uint16_t packOctahedral16(vec3 n) {
-  n /= (abs(n.x) + abs(n.y) + abs(n.z));
-  return ::packSnorm2x8((n.z >= 0.0) ? vec2(n.x, n.y) : (vec2(1.0) - abs(vec2(n.y, n.x))) * msign(vec2(n)));
+  n /= (std::abs(n.x) + std::abs(n.y) + std::abs(n.z));
+  return ::packSnorm2x8((n.z >= 0.0) ? vec2(n.x, n.y) : (vec2(1.0) - glm::abs(vec2(n.y, n.x))) * msign(vec2(n)));
 }
 
 std::vector<VertexData> vertexData_;
